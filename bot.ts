b27a013ab5c58d81ea4c1286ef675591b545eb8a
Alex Hoffman <alexhoffman617@gmail.com>
2019-10-20 13:08:01 -0400

    function to search a list for tweets with certain parameters

--- bot.ts
+++ bot.ts
@@ -2,15 +2,6 @@ var twit = require('twit')
 var config = require('./config.ts')
 var Twitter = new twit(config)
 
-var retweet = function() {
-  console.log('retweet')
-  var params = {
-    q: '#nodejs, #Nodejs',
-    result_type: 'recent',
-    lang: 'en'
-  }
-}
-
 var tweet = function(text) {
   Twitter.post('statuses/update', { status: text }, function(err, data, response) {
     if (err) {
@@ -19,4 +10,33 @@ var tweet = function(text) {
   })
 }
 
-tweet('Im posting a tweet from a bot!')
+var searchTweets = function() {
+  Twitter.get(
+    'search/tweets',
+    {
+      q: 'list:alexhoffman617/test cfp OR cfs OR "call for speakers" OR "call for proposals"',
+      result_type: 'recent',
+      lang: 'en',
+      count: 10,
+      include_entities: false
+    },
+    function(err, data, response) {
+      data.statuses.forEach(status => {
+        console.log('[' + status.user.name + '] - ' + status.text + ' **' + status.id + '**')
+      })
+    }
+  )
+}
+
+var getTimelineTweets = function() {
+  Twitter.get(
+    'statuses/home_timeline',
+    { include_entities: false, exclude_replies: true, count: 2, include_rts: 1 },
+    function(err, data, response) {
+      console.log(data)
+    }
+  )
+}
+
+// tweet('Im posting a tweet from a bot!')
+searchTweets()
